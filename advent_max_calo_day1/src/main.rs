29775bd4cad9b382eea94847d7d540da1b87ef9d
Nikolai Devolder <nikolai.devolder@gmail.com>
2022-12-01 20:49:49 +0100

    Part1 done

--- advent_max_calo_day1/src/main.rs
+++ advent_max_calo_day1/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let file: Box<dyn BufRead> = match env::args().nth(1) {
+        Some(path) => Box::new(BufReader::new(File::open(path)?)),
+        None => Box::new(BufReader::new(io::stdin())),
+    };
+
+    let mut calories: Vec<usize> = vec![0];
+    for line in file.lines() {
+        if let Ok(val) = line?.parse::<usize>() {
+            *calories.last_mut().unwrap() += val;
+        } else {
+            calories.push(0)
+        }
+    }
+
+    println!("Max is {}", calories.iter().max().unwrap());
+    Ok(())
 }
