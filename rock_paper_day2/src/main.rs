190e7aa83493548e9f0ffa44200acad478125c39
Nikolai Devolder <nikolai.devolder@gmail.com>
2022-12-03 20:14:35 +0100

    Day2

--- rock_paper_day2/src/main.rs
+++ rock_paper_day2/src/main.rs
@@ -1,3 +1,44 @@
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("../inp_ex_off.txt");
+
+    let interm = input.lines().map(|line| {
+        line.split(" ")
+            .flat_map(|c| c.chars().next())
+            .map(|x| x as isize)
+    });
+    //.flatten()
+    let a = interm
+        .clone()
+        .map(|mut x| {
+            let a = x.next().unwrap();
+            let b = x.next().unwrap();
+            let mut res = (a - b + 25) % 3;
+            if res == 1 {
+                res = 2;
+            } else if res == 2 {
+                res = 1;
+            }
+
+            b - 87 + res * 3
+        })
+        .sum::<isize>();
+
+    println!("ex1 {:?}", a);
+
+    let a = interm
+        .map(|mut x| {
+            let a = x.next().unwrap();
+            let b = x.next().unwrap();
+            let res = b - 88;
+            let mut play = a - 64 + res - 1;
+            if play == 4 {
+                play = 1;
+            } else if play == 0 {
+                play = 3;
+            }
+            res * 3 + play
+        })
+        .sum::<isize>();
+
+    println!("ex2 {:?}", a)
 }
