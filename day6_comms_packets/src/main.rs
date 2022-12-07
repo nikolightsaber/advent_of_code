3fb995cec062a885697d6907a94ae4701a95053b
Nikolai Devolder <nikolai.devolder@gmail.com>
2022-12-07 21:37:58 +0100

    day6 cheated

--- day6_comms_packets/src/main.rs
+++ day6_comms_packets/src/main.rs
@@ -1,24 +1,39 @@
 use std::error::Error;
 
+fn solve(inp: &[u8], win: usize) -> usize {
+    inp.windows(win)
+        .position(|vals| {
+            let mut data: u32 = 0;
+            for v in vals {
+                let prev = data;
+                data |= 1 << (v - b'a');
+                if data == prev {
+                    return false;
+                }
+            }
+            return true;
+        })
+        .unwrap()
+        + win
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let input = include_str!("../inp.txt");
 
-    let temp = input.lines().map(|line| line.bytes());
-    for line in temp {
-        let mut matching: Vec<u8> = Vec::new();
-        let mut out = 0;
-        for (i, b) in line.enumerate() {
-            if matching.len() == 4 {
-                if matching.iter().all(|c| *c != b) {
-                    dbg!(matching);
-                    out = i;
-                    break;
-                }
-                matching.truncate(matching.len() - 2);
-            }
-            matching.push(b);
-        }
-        println!("ex1 {}", out)
-    }
+    let out = input
+        .lines()
+        .map(|line| line.as_bytes())
+        .map(|set| solve(set, 4))
+        .collect::<Vec<usize>>();
+
+    println!("ex1 {:?}", out);
+
+    let out = input
+        .lines()
+        .map(|line| line.as_bytes())
+        .map(|set| solve(set, 14))
+        .collect::<Vec<usize>>();
+
+    println!("ex1 {:?}", out);
     Ok(())
 }
