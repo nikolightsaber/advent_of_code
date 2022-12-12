80308438c141008685e831979ef609b9a7ad16a1
Nikolai Devolder <nikolai.devolder@gmail.com>
2022-12-12 20:37:45 +0100

    Working filesystem day7

--- day7_directories/src/main.rs
+++ day7_directories/src/main.rs
@@ -2,13 +2,13 @@ use std::error::Error;
 
 #[derive(Debug)]
 struct File {
-    name: String,
+    _name: String,
     size: usize,
 }
 
 impl File {
-    fn new(name: String, size: usize) -> Self {
-        File { name, size }
+    fn new(_name: String, size: usize) -> Self {
+        File { _name, size }
     }
 }
 
@@ -17,6 +17,7 @@ struct Directory {
     name: String,
     files: Vec<File>,
     dirs: Vec<Directory>,
+    parent: Option<Box<Directory>>,
 }
 
 impl Directory {
@@ -25,15 +26,29 @@ impl Directory {
             name,
             files: Vec::new(),
             dirs: Vec::new(),
+            parent: None,
         }
     }
 
-    fn cd(&mut self, name: &str) -> Option<&mut Self> {
-        if self.name == name {
-            return Some(self);
+    fn cd(mut self, name: &str) -> Option<Self> {
+        if name == ".." {
+            let mut dir = *self.parent.take()?;
+            dir.dirs.push(self);
+            return Some(dir);
         }
 
-        self.dirs.iter_mut().find(|dir| dir.name == name)
+        if name == "/" {
+            let mut dir = self;
+            while dir.parent.is_some() {
+                dir = dir.cd("..")?;
+            }
+            return Some(dir);
+        }
+
+        let pos = self.dirs.iter().position(|dir| dir.name == name)?;
+        let mut dir = self.dirs.remove(pos);
+        dir.parent = Some(Box::new(self));
+        Some(dir)
     }
 
     fn mkdir(&mut self, dir: Directory) {
@@ -43,16 +58,44 @@ impl Directory {
     fn touch(&mut self, file: File) {
         self.files.push(file);
     }
+
+    fn du(&self) -> usize {
+        let mut size: usize = self.files.iter().map(|file| file.size).sum();
+        size += self.dirs.iter().map(|dir| dir.du()).sum::<usize>();
+        return size;
+    }
+}
+
+fn small_du(dir: &Directory) -> usize {
+    let cwd_size = dir.du();
+    let subdir_size = dir.dirs.iter().map(|dir| small_du(dir)).sum::<usize>();
+    if cwd_size > 100000 {
+        return subdir_size;
+    } else {
+        return cwd_size + subdir_size;
+    }
+}
+
+fn get_smallest(dir: &Directory, min: usize, last: usize) -> usize {
+    let cwd_size = dir.du();
+    if cwd_size < min || cwd_size > last {
+        return dir
+            .dirs
+            .iter()
+            .fold(last, |last, dir| get_smallest(dir, min, last));
+    }
+
+    return dir
+        .dirs
+        .iter()
+        .fold(cwd_size, |last, dir| get_smallest(dir, min, last));
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut root = Directory::new(String::from("/"));
-    let mut cwd = &mut root;
-    let mut last_dir: Option<&mut Directory> = None;
-    let input = include_str!("../inp_test.txt");
+    let mut cwd = Directory::new(String::from("/"));
+    let input = include_str!("../inp_off.txt");
 
     for line in input.lines() {
-        dbg!(line);
         let (left, right) = line.split_once(" ").expect("uncorrect input");
         match left {
             "$" => {
@@ -63,16 +106,7 @@ fn main() -> Result<(), Box<dyn Error>> {
                             panic!("Invalid input");
                         }
 
-                        if dir == ".." {
-                            break;
-                            cwd = last_dir.take().expect("Invalid input");
-                        }
-
-                        cwd = cwd.cd(dir).unwrap();
-                        // (cwd, last_dir) = match cwd.cd(dir) {
-                        //     (Some(c_dir), l_dir) => (c_dir, l_dir),
-                        //     _ => panic!("Invalid input"),
-                        // };
+                        cwd = cwd.cd(dir).expect(&format!("dir does not exist: {}", dir));
                     }
                     None if right == "ls" => (), // Do nothing wait for next lines
                     _ => panic!("Invalid input"),
@@ -88,7 +122,13 @@ fn main() -> Result<(), Box<dyn Error>> {
             }
         }
     }
-    dbg!(root);
+    cwd = cwd.cd("/").expect("Root must exist");
+
+    println!("ex 1 {}", small_du(&cwd));
+
+    let space_required = 30000000 - (70000000 - cwd.du());
+
+    println!("ex 2 {}", get_smallest(&cwd, space_required, usize::MAX));
 
     Ok(())
 }
