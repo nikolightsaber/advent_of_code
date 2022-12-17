63f0b71910d5ffd168a9b43a93c43c73482dc53b
Nikolai Devolder <nikolai.devolder@gmail.com>
2022-12-17 21:22:16 +0100

    This is going to be hard

--- day11_monkey/src/main.rs
+++ day11_monkey/src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
+use std::error::Error;
+use std::str::FromStr;
+
+struct Monkey {
+    items: Vec<usize>,
+    operation: Box<dyn Fn(usize) -> usize>,
+    test: Box<dyn Fn(usize) -> usize>,
+}
+
+impl FromStr for Monkey {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut lines = s.lines();
+        let (_, items) = lines.next().ok_or(())?.split_once(":").ok_or(())?;
+        let items: Vec<usize> = items
+            .split(",")
+            .flat_map(|item| item.parse::<usize>())
+            .collect();
+
+        let (_, operation) = lines.next().ok_or(())?.split_once(":").ok_or(())?;
+        let (_, test) = lines.next().ok_or(())?.split_once(":").ok_or(())?;
+        todo!();
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
     println!("Hello, world!");
+    Ok(())
 }
