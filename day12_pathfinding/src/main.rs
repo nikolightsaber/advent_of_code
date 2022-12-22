04abc2fff0dc14f0f98082a84d2666e8c15068e1
Nikolai Devolder <nikolai.devolder@gmail.com>
2022-12-22 22:14:33 +0100

    Generic type learning on day12

--- day12_pathfinding/src/main.rs
+++ day12_pathfinding/src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+struct Vec2D<T> {
+    data: Vec<T>,
+    count: usize,
+}
+
+impl<T> Vec2D<T> {
+    fn new<B: Sized>(count: usize) -> Vec2D<B> {
+        Vec2D {
+            data: Vec::<B>::new(),
+            count,
+        }
+    }
+
+    fn get(&self, x: usize, y: usize) -> Option<&T> {
+        self.data.get(x + y * self.count)
+    }
+
+    fn get_mut(&mut self, x: usize, y: usize) -> Option<&mut T> {
+        self.data.get_mut(x + y * self.count)
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let input = std::fs::read_to_string("inp_off.txt")?;
+
+    Ok(())
 }
