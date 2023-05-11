831e87b62a8badd45366fdef58c5d1c37369fce9
Nikolai Devolder <nikolai.devolder@gmail.com>
2023-05-12 00:31:04 +0200

    Something close but not there yet

--- day12_pathfinding/src/main.rs
+++ day12_pathfinding/src/main.rs
@@ -1,29 +1,83 @@
 use std::error::Error;
 
-struct Vec2D<T> {
-    data: Vec<T>,
-    count: usize,
+fn pretty_print(map: &Vec<Vec<u8>>, path: &Vec<(usize, usize)>) {
+    for (i, l) in map.iter().enumerate() {
+        for (j, c) in l.iter().enumerate() {
+            if path.contains(&(i, j)) {
+                print!("\x1b[93m{}\x1b[0m", *c as char);
+            } else {
+                print!("{}", *c as char);
+            }
+        }
+        println!();
+    }
+    println!("    {}", path.len());
 }
 
-impl<T> Vec2D<T> {
-    fn new<B: Sized>(count: usize) -> Vec2D<B> {
-        Vec2D {
-            data: Vec::<B>::new(),
-            count,
-        }
+fn search(
+    map: &Vec<Vec<u8>>,
+    index: (usize, usize),
+    prev_height: u8,
+    mut path: &mut Vec<(usize, usize)>,
+) -> Option<u8> {
+    if path.contains(&index) {
+        return None;
     }
-
-    fn get(&self, x: usize, y: usize) -> Option<&T> {
-        self.data.get(x + y * self.count)
+    //dbg!(index);
+    let height = map.get(index.0)?.get(index.1)?;
+    if prev_height != *height && prev_height + 1 != *height && prev_height - 1 != *height {
+        return None;
     }
-
-    fn get_mut(&mut self, x: usize, y: usize) -> Option<&mut T> {
-        self.data.get_mut(x + y * self.count)
+    if *height == b'z' + 1 {
+        pretty_print(map, path);
+        return Some(0);
     }
+    path.push(index);
+    [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        .into_iter()
+        .flat_map(|(i, j): (isize, isize)| {
+            Some((
+                index.0.checked_add_signed(i)?,
+                index.1.checked_add_signed(j)?,
+            ))
+        })
+        .flat_map(|i| search(map, i, *height, &mut path))
+        .min()
+        .map_or_else(
+            || {
+                path.pop();
+                None
+            },
+            |v| Some(v + 1),
+        )
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let input = std::fs::read_to_string("inp_off.txt")?;
+    let mut first = (0, 0);
+    let map: Vec<_> = input
+        .lines()
+        .enumerate()
+        .map(|(i, l)| {
+            l.bytes()
+                .enumerate()
+                .map(|(j, c)| {
+                    if c == b'S' {
+                        first = (i, j);
+                        return b'a';
+                    }
+                    if c == b'E' {
+                        return b'z' + 1;
+                    }
+                    c
+                })
+                .collect::<Vec<_>>()
+        })
+        .collect();
+
+    let mut v = Vec::<(usize, usize)>::new();
+    let a = search(&map, first, b'a', &mut v);
+    println!("{:?}", a);
 
     Ok(())
 }
