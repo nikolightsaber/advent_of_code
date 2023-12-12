effc2afc4690d327facbbe9db3eb447e6d6416f8
Nikolai Devolder <nikolai.devolder@gmail.com>
2023-12-12 22:43:44 +0100

    Trying to brute force day 5

--- day05_seed_fertilizer/src/main.rs
+++ day05_seed_fertilizer/src/main.rs
@@ -1,4 +1,13 @@
-use std::{error::Error, num::ParseIntError, str::FromStr};
+use std::{
+    error::Error,
+    future,
+    num::ParseIntError,
+    str::FromStr,
+    sync::{mpsc, Arc},
+    thread,
+};
+
+use futures::{executor::block_on, future::join_all};
 
 #[derive(Debug)]
 struct Map {
@@ -28,6 +37,9 @@ impl Adaptor {
             }
         }
         seed
+    }
+    async fn adapt_async(&self, seed: u64) -> u64 {
+        self.adapt(seed)
     }
 }
 
@@ -60,13 +72,69 @@ impl FromStr for Adaptor {
     }
 }
 
+// Litterally gets killed
+fn multi_test(array: &mut Vec<u64>, adaptor: Arc<Adaptor>) {
+    const THREADS: usize = 6;
+    let chunk_size = array.len() / THREADS;
+
+    // Create a vector to hold the thread handles
+    let mut handles = vec![];
+
+    let (tx, rx) = mpsc::channel();
+    // Spawn threads
+    for i in 0..THREADS {
+        let mut array_chunk = vec![];
+        for _ in 0..chunk_size {
+            array_chunk.push(array.pop().unwrap());
+        }
+
+        println!("spawning {}", i);
+        let ad = adaptor.clone();
+        let tx = tx.clone();
+        let handle = thread::spawn(move || {
+            println!("spawned {}", i);
+            for value in array_chunk.iter_mut() {
+                tx.send(ad.adapt(*value)).unwrap();
+            }
+        });
+
+        handles.push(handle);
+    }
+
+    // Wait for all threads to finish
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    for val in rx {
+        array.push(val);
+    }
+}
+
+// async not made for low load lot of them
+async fn multi_test2(array: &mut Vec<u64>, adaptor: &Adaptor) {
+    const THREADS: usize = 10000;
+
+    for i in (0..array.len()).step_by(THREADS) {
+        let mut handles = vec![];
+        println!("starting");
+        for j in i..std::cmp::min(i + THREADS, array.len()) {
+            handles.push(adaptor.adapt_async(array[j]))
+        }
+
+        for (k, v) in join_all(handles).await.into_iter().enumerate() {
+            array[i + k] = v;
+        }
+        println!("ending");
+    }
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let input = include_str!("official");
 
     let mut input_iter = input.split("\n\n");
-    let mut seeds = input_iter
-        .next()
-        .expect("Wrong input")
+    let seeds_str = input_iter.next().expect("Wrong input");
+    let mut seeds1 = seeds_str
         .split_once(": ")
         .map(|(_, seeds)| {
             seeds
@@ -77,12 +145,29 @@ fn main() -> Result<(), Box<dyn Error>> {
         .expect("Wrong input")
         .expect("Wrong input");
 
+    let mut seeds2 = seeds_str
+        .split_once(": ")
+        .map(|(_, seeds)| {
+            let d = seeds.split_whitespace().flat_map(str::parse::<u64>);
+            let d2 = d.clone();
+            d.step_by(2)
+                .zip(d2.skip(1).step_by(2))
+                .map(|(a, b)| a..(a + b))
+                .flatten()
+                .collect::<Vec<_>>()
+        })
+        .expect("Wrong input");
+
+    dbg!(seeds2.len());
+
     for adaptor in input_iter {
         let adaptor: Adaptor = adaptor.parse().expect("Wrong input");
-        for seed in seeds.iter_mut() {
+        for seed in seeds1.iter_mut() {
             *seed = adaptor.adapt(*seed);
         }
+        block_on(multi_test2(&mut seeds2, &adaptor));
     }
-    println!("{}", seeds.iter().min().unwrap());
+    println!("{}", seeds1.iter().min().unwrap());
+    println!("{}", seeds2.iter().min().unwrap());
     Ok(())
 }
